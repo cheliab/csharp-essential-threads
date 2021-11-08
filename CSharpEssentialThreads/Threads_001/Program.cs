8028aaa485892b0a79d0b5d16979d7a37f5cd002
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-08 10:12:42 +1000

    Пример - 1: Бесконечные циклы в двух потоках

--- CSharpEssentialThreads/Threads_001/Program.cs
+++ CSharpEssentialThreads/Threads_001/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Threading;
 
 namespace Threads_001
 {
+    /// <summary>
+    /// Простой пример выполнения метода в отдельном потоке
+    /// </summary>
     class Program
     {
+        // Метод, который будет запущен в отдельном потоке
+        static void WriteSecond()
+        {
+            // бесконечный цикл во втором потоке
+            while (true)
+            {
+                Console.WriteLine(new string(' ', 10) + "Secondary");                
+            }
+        }
+        
         static void Main(string[] args)
         {
+            ThreadStart writeSecond = new ThreadStart(WriteSecond);
+            Thread thread = new Thread(writeSecond);
+            thread.Start();
+
+            // бесконечный цикл в основном потоке
+            while (true)
+            {
+                Console.WriteLine("Primary");
+            }
+            
+            // до этого места программа не дойдет
             Console.ReadLine();
+            
+            
         }
     }
 }
+
+// Результат:
+//         Secondary
+//         Secondary
+//         Secondary
+// Primary
+// Primary
+// Primary
