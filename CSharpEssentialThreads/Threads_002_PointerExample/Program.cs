b03ae706de2526166bb94f34782063e2a2b30349
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-10 23:19:15 +1000

    Добавил пример с указателями локальных переменных

---
+++ CSharpEssentialThreads/Threads_002_PointerExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_002_PointerExample
+{
+    class Program
+    {
+        private static void GetPointerInfo()
+        {
+            unsafe
+            {
+                var localVar = 1;
+                var localPointer = &localVar; // адресс локальной переменной в памяти 
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(GetPointerInfo);
+            thread.Start();
+
+            GetPointerInfo();
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpEssentialThreads/Threads_002_PointerExample/README.md
@@ -0,0 +1,5 @@
+﻿Адрес переменной в основном потоке:
+![Primary thread](img/Main%20-%20local%20variable%20pointer.png)
+
+Адрес переменной во втором потоке:
+![Second thread](img/Second%20thread%20-%20local%20variable%20pointer.png)
