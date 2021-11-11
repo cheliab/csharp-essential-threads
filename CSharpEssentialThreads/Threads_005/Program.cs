b78841018f777ac186b6453156334239bd30d6d2
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-11 23:47:04 +1000

    Добавил пример Threads_005 - Использование делегата и лямбда выражения
    Плюс передача параметра в тело функции (замыкания)

---
+++ CSharpEssentialThreads/Threads_005/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads_005
+{
+    /// <summary>
+    /// Использование анонимных методов и лямбда выражений
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            
+            Thread threadOne = new Thread(delegate() { Console.WriteLine($"1. counter = {++counter}"); });
+            threadOne.Start();
+            
+            Thread.Sleep(100);
+            Console.WriteLine($"2. counter = {counter}");
+
+            Thread threadTwo = new Thread((object argument) => { Console.WriteLine($"3. counter = {(int)argument}"); });
+            threadTwo.Start(counter);
+            
+            Console.ReadLine();
+        }
+    }
+}
