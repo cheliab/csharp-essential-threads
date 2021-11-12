0b8bafcec0d7a1afed17d9e0056ccf89268393cc
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-12 22:07:41 +1000

    Набросок примера CriticalSection_001 - Использование lock

---
+++ CSharpEssentialThreads/CriticalSection_001/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+// Критичейская секция (critical section)
+
+// lock - блокирует участов кода, чтобы его мог выполнить только 1 поток
+// остальные потоки будут ждать
+
+namespace CriticalSection_001
+{
+    class ClassWithCriticalSection
+    {
+        /// <summary>
+        /// Объект блокировки (объект синхронизации доступа к разделяемуму ресурсу)
+        /// </summary>
+        private object block = new object();
+
+        public void MethodWithCriticalSection()
+        {
+            int threadHash = Thread.CurrentThread.GetHashCode();
+
+            lock (block)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine();
+                    
+                    Thread.Sleep(100);
+                }
+                
+                Console.WriteLine(new string('-', 20));
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
