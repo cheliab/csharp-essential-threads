da5326ff018d4e9b737704a1ed0a883ccdf33822
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-14 00:24:13 +1000

    Добавил пример CriticalSection_002 - Использование Monitor вместо lock
    lock это просто синтаксичейский сахар для класса Monitor

---
+++ CSharpEssentialThreads/CriticalSection_002/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+
+namespace CriticalSection_002
+{
+    class ClassWithMonitor
+    {
+        private object locker = new object();
+
+        public void MethodWithMonitor()
+        {
+            int threadHash = Thread.CurrentThread.GetHashCode();
+            
+            Monitor.Enter(locker);
+
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Поток № {threadHash}: шаг {counter}");
+                Thread.Sleep(200);
+            }
+            Console.WriteLine(new string('-', 20));
+            
+            Monitor.Exit(locker);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classWithMonitor = new ClassWithMonitor();
+            
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(classWithMonitor.MethodWithMonitor).Start();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
+
+// Результат:
+// Поток № 4: шаг 0
+// Поток № 4: шаг 1
+// Поток № 4: шаг 2
+// Поток № 4: шаг 3
+// Поток № 4: шаг 4
+// Поток № 4: шаг 5
+// Поток № 4: шаг 6
+// Поток № 4: шаг 7
+// Поток № 4: шаг 8
+// Поток № 4: шаг 9
+// --------------------
+// Поток № 6: шаг 0
+// Поток № 6: шаг 1
+// Поток № 6: шаг 2
+// Поток № 6: шаг 3
+// Поток № 6: шаг 4
+// Поток № 6: шаг 5
+// Поток № 6: шаг 6
+// Поток № 6: шаг 7
+// Поток № 6: шаг 8
+// Поток № 6: шаг 9
+// --------------------
+// Поток № 5: шаг 0
+// Поток № 5: шаг 1
+// Поток № 5: шаг 2
+// Поток № 5: шаг 3
+// Поток № 5: шаг 4
+// Поток № 5: шаг 5
+// Поток № 5: шаг 6
+// Поток № 5: шаг 7
+// Поток № 5: шаг 8
+// Поток № 5: шаг 9
+// --------------------
