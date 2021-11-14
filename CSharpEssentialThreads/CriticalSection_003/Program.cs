fef4b34e5da27ecf89f894d07ac61b8bdd9f6b1a
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-14 16:41:43 +1000

    Накидал пример CriticalSection_003 - Использование одного объекта блокировки в нескольких lock конструкциях

---
+++ CSharpEssentialThreads/CriticalSection_003/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+// Синхронизация в цикле
+namespace CriticalSection_003
+{
+    class Program
+    {
+        private static object locker = new object(); // один объект синхронизации используется в разных потоках в цикле
+
+        private static void WriteSecond()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                lock (locker)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(new string(' ', 10) + "Secondary");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 45);
+
+            Thread thread = new Thread(WriteSecond);
+            thread.Start();
+
+            for (int i = 0; i < 20; i++)
+            {
+                lock (locker)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Primary");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    
+                    Thread.Sleep(100);    
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
